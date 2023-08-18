25e9fde7a8671d5d049f87154831d59575a46981
Elena Min <elena.min@testpro.io>
2023-08-18 14:31:05 -0400

    changes

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -1,28 +1,29 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class LoginTests extends BaseTest {
+public class Homework16 extends BaseTest{
+
     @Test
-    public void loginEmptyEmailPassword() {
-//      Added ChromeOptions argument below to fix websocket error
+
+    public void registrationNavigation() {
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--start-maximized");
-
-        WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         String url = "https://qa.koel.app/";
         driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
 
-        driver.quit();
+        WebElement registrationLink = driver.findElement(By.ByCssSelector("[href='registration']"));
+
+        String registrationLink = "https://qa.koel.app/registration";
+
+        Assert.assertEquals(driver.getCurrentUrl(), registrationLink);
+
     }
 }
