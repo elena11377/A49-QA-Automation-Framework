f44b3c62a59ed605530204c4eb492677f6f819bb
Elena Min <elena.min@testpro.io>
2023-08-18 14:54:03 -0400

    HW16-registration
    registration link

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -23,4 +23,26 @@ public class LoginTests extends BaseTest {
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
     }
+
+    @Test
+    public void registrationNavigation() {
+//Given Open the browser
+        //      Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+// When I open my url link
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+
+        // AND navigate ti the registration button
+        WebElement registrationLink = driver.findElement(By.cssSelector("[href='registration']"));
+        //AND I click on "registration"
+        registrationLink.click();
+// THEN registration page displayed
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+        driver.quit();
+    }
 }
