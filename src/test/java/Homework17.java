6a486f878011143315cd0905ee05ca3ac4e10171
Elena Min <elena.min@testpro.io>
2023-08-24 17:43:58 -0400

    Elena Min - HW17-test passed

---
+++ src/test/java/Homework17.java
@@ -0,0 +1,54 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework17 extends BaseTest {
+
+    @Test(description = "Homework 17 - addSongToPlaylist")
+    public void addSongToPlaylist() {
+        loginWithCorrectCreds();
+        enterSongIntoSearchInput();
+        clickViewAllBtn();
+        clickOnSongTitle();
+        clickOnAddToBtn();
+        enterNewUniquePlaylistName();
+        verifySuccessMessage();
+        getAddToPlaylistMessage();
+    }
+
+    public void verifySuccessMessage() {
+        WebElement successBanner = driver.findElement(By.cssSelector(".success"));
+        Assert.assertTrue(successBanner.isDisplayed());
+    }
+
+    public void enterNewUniquePlaylistName() {
+        enterText(By.cssSelector("#songResultsWrapper [data-test='new-playlist-name']"), generateRandomName());
+        WebElement submitBtn =  driver.findElement(By.cssSelector("#songResultsWrapper [type='submit']"));
+        submitBtn.click();
+    }
+
+    public void clickOnAddToBtn() {
+        WebElement addToBtn = driver.findElement(By.cssSelector(".btn-add-to"));
+        addToBtn.click();
+    }
+
+    public void clickOnSongTitle() {
+        WebElement songTitle = driver.findElement(By.cssSelector(".search-results .song-item .title"));
+        songTitle.click();
+    }
+
+    public void enterSongIntoSearchInput() {
+        enterText(By.cssSelector("[type='search']"), "Reactor");
+    }
+
+    public void clickViewAllBtn() {
+        WebElement viewAllBtn = driver.findElement(By.cssSelector("[data-test='view-all-songs-btn']"));
+        viewAllBtn.click();
+    }
+
+    public void getAddToPlaylistMessage(){
+        WebElement notification = driver.findElement(By.cssSelector("div.success.show"));
+    }
+
+}
