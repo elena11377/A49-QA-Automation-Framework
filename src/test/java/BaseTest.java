6d7635d00be57f424b3a5d8ebbd9d56d519ed2c1
Elena Min <elena.min@testpro.io>
2023-09-07 22:18:11 -0400

    HW21

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,131 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+import java.util.UUID;
+
+public class BaseTest {
+    public WebDriver driver = null;
+    //public String url = "https://qa.koel.app/";
+    public WebDriverWait wait = null; //HW20
+    public String url;
+    public Actions action = null;
+
+    @BeforeSuite
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+    @Parameters({"BaseURL"})
+    public void launchBrowser(String baseURL) {
+        //Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--start-maximized");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // HW20
+        actions = new Actions(driver);
+
+        url = baseURL;
+        navigateToPage();
+    }
+
+    @AfterMethod
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+    public void navigateToPage() {
+        driver.get(url);
+    }
+
+    public void provideEmail(String email) {
+        // WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='email']"))); //HW20
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+
+    public void providePassword(String password) {
+        // WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='password']"))); //HW20
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+
+    public void clickSubmit() {
+        //WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+        WebElement submit = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[type='submit']"))); //HW20
+        submit.click();
+    }
+
+    @DataProvider(name="IncorrectLoginData")
+    public static Object[][] getDataFromDataProviders(){
+        return new Object [][]{
+                {"nonExisting@gmail.com", "nonExistingPassword"},
+                {"elena.min@testpro.io", " "},
+                {"",""},
+        };
+    }
+
+
+//    public void clickAvatarIcon() {
+//        WebElement avatarIcon = driver.findElement(By.cssSelector("img.avatar"));
+//        avatarIcon.click();
+//    }
+//
+//    public void provideCurrentPassword(String password) {
+//        WebElement currentPassword = driver.findElement(By.cssSelector("[name='current_password']"));
+//        currentPassword.clear();
+//        currentPassword.sendKeys(password);
+//    }
+//
+//    public void clickSaveButton() {
+//        WebElement saveButton = driver.findElement(By.cssSelector("button.btn-submit"));
+//        saveButton.click();
+//    }
+//
+//    public void provideProfileName(String randomName) {
+//        WebElement profileName = driver.findElement(By.cssSelector("[name='name']"));
+//        profileName.clear();
+//        profileName.sendKeys(randomName);
+//    }
+//
+//    public void isAvatarDisplayed() {
+//        WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
+//        Assert.assertTrue(avatarIcon.isDisplayed());
+//    }
+//
+//    public String generateRandomName() {
+//        return UUID.randomUUID().toString().replace("-", "");
+//    }
+//
+//    protected void loginWithCorrectCreds() {
+//        navigateToPage();
+//        //provideEmail("demo@class.com");
+//        provideEmail("elena.min@testpro.io");
+//        providePassword("te$t$tudent");
+//        clickSubmit();
+//    }
+//
+//    protected void enterText(By inputLocator, String inputText) {
+//        WebElement searchInput = driver.findElement(inputLocator);
+//        searchInput.click();
+//        searchInput.clear();
+//        searchInput.sendKeys(inputText);
+//    }
+}
